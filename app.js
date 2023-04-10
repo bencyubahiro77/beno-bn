b99a0c1e4bb3abcefff5f46029558b1bec5b63fa
cyubahiro benjamin <bencyubahiro77@gmail.com>
2023-04-10 11:37:16 +0200

    added brand and category

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ const PORT = process.env.PORT || 5000;
 
 const authRouter = require("./routes/authRoute");
 const productRouter = require("./routes/productRoute");
-const blogRouter = require("./routes/blogRoute")
-
+const blogRouter = require("./routes/blogRoute");
+const pCategoryRouter = require("./routes/prodcategoryRoute")
+const blogCategoryRouter = require("./routes/blogCategoryRoute")
+const brandRouter = require("./routes/brandRoute");
 
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
@@ -21,9 +23,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 
+
 app.use("/user", authRouter);
 app.use("/product", productRouter);
 app.use("/blog", blogRouter);
+app.use("/pcategory", pCategoryRouter);
+app.use("/bcategory", blogCategoryRouter);
+app.use("/brand", brandRouter);
+
 
 app.use(notFound);
 app.use(errorHandler);
